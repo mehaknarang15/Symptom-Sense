cba14e2b50418908f183a4f65a8dde1aae1ffbc9
Komal Nama <komalnama132@gmail.com>
2025-06-19 09:01:24 +0530

    vitalScript.js

--- static/scripts/vitalScript.js
+++ static/scripts/vitalScript.js
@@ -1,21 +1,18 @@
 const header = document.querySelector("header");
-const menu = document.querySelector('#menu-icon');
-const navbar = document.querySelector('.navbar');
 
 window.addEventListener("scroll", function(){
     header.classList.toggle("sticky", window.scrollY > 0);
 });
+
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
 
 menu.onclick = () => {
     menu.classList.toggle('bx-x');
     navbar.classList.toggle('open');
 };
 
-// Close menu when any nav link is clicked
-const navLinks = document.querySelectorAll('.navbar a');
-navLinks.forEach(link => {
-    link.addEventListener('click', () => {
-        menu.classList.remove('bx-x');
-        navbar.classList.remove('open');
-    });
-});
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navbar.classList.remove('open');
+};
