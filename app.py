22c41390c8a4e37844640a432718fa27b954b14b
mehaknarang15 <mehaknarang1512@gmail.com>
2025-07-13 11:38:50 +0530

    fixed mobile layout

--- app.py
+++ app.py
@@ -141,4 +141,3 @@ def google_maps_proxy():
     except Exception as e:
         app.logger.error(f"Error proxying Google Maps API: {str(e)}")
         return "console.error('Failed to load Google Maps');", 500, {'Content-Type': 'application/javascript'}
-
